4daff8661d40de99fcfe43a15959c8b6d509650e
gabrielFPG <gfg.pantoja@yavirac.edu.ec>
2024-05-31 09:02:46 -0500

    123

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  private http = inject(HttpClient)
+
+  LoginForm=new FormGroup({
+    email:new FormControl(" ", [Validators.email, Validators.required]), //valores que quedan por defecto....en la clase
+    password:new FormControl("", Validators.required)
+  })
+
+  funIngresar(){
+    this.http.get("http://jsonplaceholder.typicode.com/todos/1").subscribe(
+      (res)=>{
+        console.log(res)
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+  
+    alert("ingresando....")
+  }
 }
